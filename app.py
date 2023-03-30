96ca91e9be796ffd8aef6192e56015a1771a0631
noafoucoux <Noa.foucoux@student.umons.ac.be>
2023-03-30 15:58:51 +0200

    end tp

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from github import Github, GithubIntegration
 
 app = Flask(__name__)
 
-app_id = '<your_app_number_here>'
+app_id = '311871'
 
 # Read the bot certificate
 with open(
@@ -23,10 +23,61 @@ def issue_opened_event(repo, payload):
     issue = repo.get_issue(number=payload['issue']['number'])
     author = issue.user.login
 
+    # Add labels to the issue
+    issue.add_to_labels('needs triage')
+    issue.add_to_labels('pending')
+
     
     response = f"Thanks for opening this issue, @{author}! " \
                 f"The repository maintainers will look into it ASAP! :speech_balloon:"
     issue.create_comment(f"{response}")
+
+def pull_request_accepted_event(repo, payload):
+    pull_request = repo.get_pull(number=payload['pull_request']['number'])
+    author = pull_request.user.login
+
+    # add comment to pull request
+
+
+    
+
+    
+    response = f"Thanks for opening this pull request, @{author}! " \
+                f"The repository maintainers will look into it ASAP! :speech_balloon:"
+    pull_request.create_issue_comment(f"{response}")
+
+    #get branch name
+    branch_name = pull_request.head.ref
+    
+    
+
+    if (pull_request.merged):
+
+        pull_request.create_issue_comment(f"Thanks for your contribution, @{author}! :tada:")
+        print (branch_name)
+
+        repo.get_git_ref(f"heads/{branch_name}").delete()
+        
+def pull_request_pending(repo, payload):
+    pull_request = repo.get_pull(number=payload['pull_request']['number'])
+    author = pull_request.user.login
+
+    # add comment to pull request
+    #get title of pull request
+    title = pull_request.title.lower()
+
+    pull_request.create_issue_comment(f"Test ")
+
+    if "wip" in title or "work in progress" in title or "do not merge" in title :
+        
+        repo.get_commit(sha=pull_request.head.sha).create_status(state='pending', description='Work in progress', context='WIP')
+        
+    else :
+        repo.get_commit(sha=pull_request.head.sha).create_status(state='success', description='Ready to merge', context='WIP')
+
+
+
+
 
 @app.route("/", methods=['POST'])
 def bot():
@@ -48,7 +99,13 @@ def bot():
     # Check if the event is a GitHub issue creation event
     if all(k in payload.keys() for k in ['action', 'issue']) and payload['action'] == 'opened':
         issue_opened_event(repo, payload)
-
+    #check if pull request accepted
+    elif all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'opened':
+        pull_request_pending(repo, payload)
+    elif all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'edited':
+        pull_request_pending(repo, payload)
+    elif all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'closed':
+        pull_request_accepted_event(repo, payload)
     return "", 204
 
 if __name__ == "__main__":
